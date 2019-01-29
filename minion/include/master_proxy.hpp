e714f54b41c74f9cac22cb2cce800cc6b459ed0e
Guy Shaibi <gshaibi@gmail.com>
2019-01-29 14:47:31 +0200

    Before tests

--- minion/include/master_proxy.hpp
+++ minion/include/master_proxy.hpp
@@ -16,18 +16,25 @@ class Minion;
 class MasterProxy : boost::noncopyable
 {
 public:
-	explicit MasterProxy(ilrd::Reactor& r_, Minion& m_);
-	// generated dtor
+	explicit MasterProxy(ilrd::Reactor& r_, Minion& m_, const sockaddr_in& vdrAddr_);
+	~MasterProxy() noexcept;
 
-	void ReplyRead(const ilrd::protocols::ID& id_, int status_, boost::shared_ptr< std::vector<char> > data_);
+	void ReplyRead(const ilrd::protocols::ID& id_, int status_, boost::shared_ptr< std::vector<char> > dataPtr_);
 	void ReplyWrite(const ilrd::protocols::ID& id_, int status_);
 
 private:
 	void OnPacketCB(int fd_);
+	void UnknownRequestImp();
+	void SendReplyImp(const ilrd::protocols::minionUDP::reply& rep_);
+	void ConstructReplyImp(ilrd::protocols::minionUDP::reply* rep_, const ilrd::protocols::ID& id_, 
+														ilrd::protocols::minionUDP::RequestType type_, int status_);
 
-	static const int SERVER_UDP_PORT = 3000;
+	static const int INCOME_UDP_PORT = 3000;
 	
+	ilrd::Reactor& m_reactor;
 	Minion& m_minion;
+	int m_udpSock;
+	sockaddr_in m_vdrAddr;
 };
 
 } // namespace minion
