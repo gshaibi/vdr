1e0ea53b8ffcabed78ab0e234d3b2ab034d28a39
Guy Shaibi <gshaibi@gmail.com>
2019-01-29 11:49:55 +0200

    Initial

--- framework/timer/timer.hpp
+++ framework/timer/timer.hpp
@@ -1,6 +1,36 @@
-#ifndef timer_hpp
-#define timer_hpp
+#ifndef TIMER_HPP
+#define TIMER_HPP
+
+#include <boost/noncopyable.hpp>
+#include <boost/chrono.hpp>
+#include <boost/function.hpp>
+
+#include "reactor.hpp"
+
+namespace ilrd
+{
+
+// P - parameter to callback. should be copyable.
+class Timer : boost::noncopyable
+{
+public:
+	explicit Timer(Reactor& r_);
+	//generated dtor.
+
+	//returns handle to be used in Cancel.
+	typedef int Handle;
+
+	Handle Set(const boost::chrono::steady_clock::duration& duration_, boost::function<void ()> callback_);
+	void Cancel(Handle handle_);
+
+private:
+	
+	Reactor& m_r;
+};
+
+} // ilrd
 
 
 
-#endif // timer_hpp
+
+#endif // TIMER_HPP
