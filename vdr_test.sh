35006f123969836fff9031dc2f1665769a66beee
Guy Shaibi <gshaibi@gmail.com>
2019-01-29 18:13:10 +0200

    Working Remote

--- vdr_test.sh
+++ vdr_test.sh
@@ -9,10 +9,12 @@ echo "Testing vdr for nbd number $1"
 
 sudo modprobe nbd
 make vdr
+make minion
 echo 4 | sudo tee /sys/block/nbd$1/queue/max_sectors_kb 1> /dev/null
 sudo ./minion.out 1 2&
+sleep 1
 sudo ./vdr.out /dev/nbd$1 10000&
-sleep 2
+sleep 1
 sudo mkfs.ext4 /dev/nbd$1
 
 if ! sudo mount /dev/nbd$1 /mnt
