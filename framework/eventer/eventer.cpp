5b33bf6ee011e3a05187c2f03ddb87fb32eae54d
chalil.weizer <halilvzr@gmail.com>
2019-01-30 16:23:55 +0200

    eventer for review before tests

--- framework/eventer/eventer.cpp
+++ framework/eventer/eventer.cpp
@@ -1,21 +1,128 @@
-#include <cassert> // assert //TODO: necessary?
+#include <cassert> // assert
+#include <unistd.h> // using pipe , write , read
+#include <fcntl.h> // using O_NONBLOCK
+
+#include <algorithm> //using remove
+#include <sstream> //using stingstream
+
+#include <boost/bind.hpp> //using boost bind 
 
 #include "eventer.hpp" // header file
+#include "logger.hpp" //using logger
 
 namespace ilrd
 {
-
 /**********************************Eventer*************************************/
 //ctors & dtors//
+Eventer::Eventer(Reactor& reactor_):
+									m_reactor(reactor_),
+									m_events(),
+									m_handleCounter(0),
+									m_eventsLock()
+{
+	//create pipe
+	//save both ends of the pipe (in m_pipe)
+	if(-1 == pipe2(m_pipe, O_NONBLOCK))
+	{
+		ilrd::Log("[Eventer] create pipe failed");
+		throw std::runtime_error(strerror(errno));
+	}
+}
 
+Eventer::~Eventer()
+{
+	//close pipes resource
+	close(m_pipe[READ]);
+	close(m_pipe[WRITE]);
+}
 
 //methods//
+Eventer::Handle Eventer::SetEvent(boost::function<void(void)> cb_)
+{
+	//Lock before set new event
+	boost::unique_lock<boost::mutex> lock(m_eventsLock);
 
+	ilrd::Log("[Eventer] set event");
+	
+	//create a key 
+	Handle usrHandle = m_handleCounter++;
+	
+	//insert to key to m_events along with the callback as value
+	
+		//Concurrently accessing other elements is safe.
+		//concurrently iterating ranges in the container is not safe.
+	m_events[usrHandle] = cb_;
+	
+	//register to reactor with the READ side of pipe 
+	m_reactor.AddFD(m_pipe[READ], Reactor::READ, boost::bind(&Eventer::OnEventFinishedCB, this, _1));
+	
+	{
+		std::stringstream msg;
+		msg << "[Eventer] event id is " << usrHandle <<std::endl;
+		ilrd::Log(msg.str());
+	}
 
-//member operators//
+	//return the key as handle to the user
+	return usrHandle;
+}
 
+void Eventer::SignalEvent(Handle handle_) const
+{
+	//TODO:Lock before write
+	boost::unique_lock<boost::mutex> lock(m_eventsLock);
+
+	ilrd::Log("[Eventer] Signal event");
+	//write the events key number (handle_) into side b of the pipe
+	
+	if(-1 == write(m_pipe[WRITE], &handle_, sizeof(Handle)))
+	{
+		ilrd::Log("[Eventer] write to pipe failed");
+		throw std::runtime_error(strerror(errno));
+	}
+	
+	{
+		std::stringstream msg;
+		msg << "[Eventer] event id " << handle_ <<" signaled"<< std::endl;
+		ilrd::Log(msg.str());
+	}
+}
 
 //private methods//
+void Eventer::OnEventFinishedCB(int readFd_) //callback function
+{
+	ilrd::Log("[Eventer] on event finished");
+	Handle usrHandle = 0;
+	
+	// read the key from the pipe 
+	if(-1 == read(readFd_, &usrHandle, sizeof(Handle)))
+	{
+		ilrd::Log("[Eventer] read from pipe failed");
+		throw std::runtime_error(strerror(errno));
+	}
+
+	{
+		std::stringstream msg;
+		msg << "[Eventer] invoking event id " << usrHandle <<" callback"<< std::endl;
+		ilrd::Log(msg.str());
+	}
+
+	//invoke the event callback
+	//Concurrently accessing other elements is safe.
+	//concurrently iterating ranges in the container is not safe.
+	m_events[usrHandle]();
+
+	//Lock before remove
+	boost::unique_lock<boost::mutex> lock(m_eventsLock);
+
+	//remove the event element from the map
+	m_events.erase(usrHandle);
+	
+	{
+		std::stringstream msg;
+		msg << "[Eventer] event id " << usrHandle <<" removed"<< std::endl;
+		ilrd::Log(msg.str());
+	}
+}
 
 } //namespace ilrd
 
--- framework/include/eventer.hpp
+++ framework/include/eventer.hpp
@@ -1,30 +1,47 @@
 #ifndef EVENTER
 #define EVENTER
 
-#include <boost/noncopyable.hpp> 
+#include <map> //using map
 
-#include "routines.hpp"
+#include <boost/noncopyable.hpp>  // using boost noncopyable
+#include <boost/function.hpp> //using boost function
+#include <boost/thread/mutex.hpp> //using mutex
+
 #include "reactor.hpp" //using Reactor
+#include "routines.hpp"
 
 namespace ilrd
 {
 
-// TODO: API  NOT FINISHED
+//TODO: comment on thread saftey guarantees
+
 class Eventer : private boost::noncopyable
 {
 public:
   //constructors & destructors//
   explicit Eventer(Reactor& reactor_); 
-	// using generated Dtor. Blocked CCtor & op=
+	~Eventer();
+
+	// Blocked CCtor & op=
 
   //methods//
-	typedef int Handle;
+	typedef unsigned int Handle;
 	Handle SetEvent(boost::function<void(void)> cb_);
-	void SignalEvent(Handle handle_);
+	void SignalEvent(Handle handle_) const;
+
 private:
+	enum PipeType{READ = 0, WRITE = 1};
+
+	typedef boost::function<void(void)> CallBack;
+	void OnEventFinishedCB(int fd_); //callback function
+
 	Reactor& m_reactor;
+	std::map<Handle, CallBack> m_events;
+	int m_pipe[2];
+	Handle m_handleCounter;
+	mutable boost::mutex m_eventsLock;
+
 };//class Eventer
-/******************************************************************************/
 
 } //namespae ilrd
 
