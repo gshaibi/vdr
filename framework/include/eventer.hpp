3b6e6bc9bc4e2d9c0c3ae188bbc233aeb63e3afe
tuval.kohl <tuvalkohl@gmail.com>
2019-01-30 09:12:18 +0200

    eventer hpp not finished

--- framework/include/eventer.hpp
+++ framework/include/eventer.hpp
@@ -4,21 +4,25 @@
 #include <boost/noncopyable.hpp> 
 
 #include "routines.hpp"
+#include "reactor.hpp" //using Reactor
 
 namespace ilrd
 {
 
+// TODO: API  NOT FINISHED
 class Eventer : private boost::noncopyable
 {
 public:
   //constructors & destructors//
-  explicit Eventer(); 
-  ~Eventer() noexcept; //TODO: need dtor?
+  explicit Eventer(Reactor& reactor_); 
+	// using generated Dtor. Blocked CCtor & op=
 
   //methods//
-
+	typedef int Handle;
+	Handle SetEvent(boost::function<void(void)> cb_);
+	void SignalEvent(Handle handle_);
 private:
-
+	Reactor& m_reactor;
 };//class Eventer
 /******************************************************************************/
 
