1e3de2cd166872431acabba9714287bc97f861d6
chalil.weizer <halilvzr@gmail.com>
2019-02-03 19:20:30 +0200

    encryptor passed basic tests - log added to functions

--- framework/encryptor/encryptor.cpp
+++ framework/encryptor/encryptor.cpp
@@ -1,6 +1,7 @@
 #include <boost/bind.hpp> // using bind
 
 #include "encryptor.hpp" //header file
+#include "logger.hpp"
 
 namespace ilrd
 {
@@ -10,33 +11,36 @@ Encryptor::Encryptor(Eventer& eventer_, ThreadPool& threadPool_):
 											m_eventer(eventer_),
 											m_threadPool(threadPool_)
 {
-	//start thread pool in case not running?
-	//TODO: throw in case its not running
 }
 
 //methods
 void Encryptor::Encrypt(CallBack cb_, Buffer buff_)
 {
+	ilrd::Log("[Encryptor] got Encryption request");
 	SendTaskToThreadPoolIMP(cb_, buff_);	
 }
 
 void Encryptor::Decrypt(CallBack cb_, Buffer buff_)
 {
+	ilrd::Log("[Encryptor] got Decryption request");
 	SendTaskToThreadPoolIMP(cb_, buff_);
 }
 
 //methods
 void Encryptor::SendTaskToThreadPoolIMP(CallBack cb_, Buffer buff_)
 {
+	ilrd::Log("[Encryptor] setting new event");
 	//set event
 	Eventer::Handle handle = m_eventer.SetEvent(cb_); //thread safe function
 	
+	ilrd::Log("[Encryptor] add encryption task to thread pool");
 	//add task to thread pool (using default priority : MEDIUM)
 	m_threadPool.Add(boost::bind(&Encryptor::EncryptDecryptIMP, this, handle, buff_)); //thread safe function
 }
 
 void Encryptor::EncryptDecryptIMP(Eventer::Handle handle_, Buffer buff_)
 {
+	ilrd::Log("[Encryptor] stating encryption/decryption task");
 	char key = 'K'; //Any char will work
     
 	size_t size = buff_->size();
@@ -46,6 +50,8 @@ void Encryptor::EncryptDecryptIMP(Eventer::Handle handle_, Buffer buff_)
         buff_->at(i) ^= key;
 	}
 
+	ilrd::Log("[Encryptor] encryption/decryption task finished");
+	ilrd::Log("[Encryptor] signaling event");
 	//signal event completed
 	m_eventer.SignalEvent(handle_);
 }
