6be94ed8442ab833768244aa237eba90f30e8366
Guy Shaibi <gshaibi@gmail.com>
2019-02-04 14:12:57 +0200

    init

--- vdr/include/block_table.hpp
+++ vdr/include/block_table.hpp
@@ -2,18 +2,17 @@
 #define BLOCK_TABLE_HPP
 
 #include <vector> //using std::vector
+#include <unordered_map>
 
 #include <boost/noncopyable.hpp> //using boost::noncopyable
 
 namespace ilrd
 {
 
-
-
 class BlockTable : private boost::noncopyable
 {
 public:
-	BlockTable(size_t blockSize_);
+	BlockTable(size_t blockSize_, size_t numBlocks_ = 0, size_t numMinions_ = 0);
 	
 	struct BlockLocation
 	{
@@ -23,9 +22,20 @@ public:
 
 	std::vector<BlockLocation> Translate(size_t offset_) const;
 
-
 private:
+	typedef size_t BlockGroup;
+	typedef size_t MinionID;
+	typedef size_t Block;
+
 	size_t m_blockSize;
+
+	struct BlockGroupLocation
+	{
+		MinionID m_minion;
+		Block m_begin;
+	};
+	
+	std::unordered_multimap<BlockGroup, BlockGroupLocation> m_blockGroup2Location;
 };//class BlockTable
 
 } // namespace ilrd
